c3b6883a9739349c4d30d4e97811473bf0f3f741
Shool User <user@shool.edu>
2026-06-10 22:28:58 +0800

    Optimize webpack chunks for Cloudflare Pages 25MB limit

--- next.config.js
+++ next.config.js
@@ -3,16 +3,51 @@ const nextConfig = {
   images: {
     domains: [],
   },
+  experimental: {
+    optimizePackageImports: ['lucide-react', 'framer-motion'],
+  },
   webpack: (config, { isServer }) => {
     config.resolve.fallback = { fs: false, net: false, tls: false }
     
-    config.optimization = {
-      ...config.optimization,
-      splitChunks: {
+    if (!isServer) {
+      config.optimization.splitChunks = {
         chunks: 'all',
-        minSize: 20000,
-        maxSize: 25000000,
-      },
+        minSize: 10000,
+        maxSize: 5000000,
+        cacheGroups: {
+          default: false,
+          vendors: false,
+          commons: {
+            name: 'commons',
+            chunks: 'all',
+            minChunks: 2,
+          },
+          lib: {
+            test: /[\\/]node_modules[\\/](react|react-dom|scheduler|prop-types|hoist-non-react-statics)[\\/]/,
+            name: 'vendors-react',
+            chunks: 'all',
+            priority: 40,
+          },
+          framer: {
+            test: /[\\/]node_modules[\\/]framer-motion[\\/]/,
+            name: 'vendors-framer',
+            chunks: 'all',
+            priority: 30,
+          },
+          lucide: {
+            test: /[\\/]node_modules[\\/]lucide-react[\\/]/,
+            name: 'vendors-lucide',
+            chunks: 'all',
+            priority: 30,
+          },
+          next: {
+            test: /[\\/]node_modules[\\/]next[\\/]/,
+            name: 'vendors-next',
+            chunks: 'all',
+            priority: 20,
+          },
+        },
+      }
     }
     
     return config
